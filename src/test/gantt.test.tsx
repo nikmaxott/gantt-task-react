6b8740cc80cd3b863c6f4b198555f71c166839e3
Nikolaus Zolnhofer <nikolaus.zolnhofer@aisus.co.uk>
2024-08-01 08:56:00 +0100

    Mock SVG Elements as JSDom does not currently support them properly

--- src/test/gantt.test.tsx
+++ src/test/gantt.test.tsx
@@ -2,6 +2,46 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Gantt } from "../index";
 
+// Extend SVGElement interface to satisfy typescript
+interface ExtendedSVGElement extends SVGElement {
+  createSVGPoint: () => SVGPoint;
+  getBBox: () => DOMRect;
+}
+
+// Mock createSVGPoint method
+beforeAll(() => {
+  (SVGElement.prototype as ExtendedSVGElement).createSVGPoint = function () {
+    return {
+      x: 0,
+      y: 0,
+      matrixTransform: function () {
+        return this;
+      },
+      w: 0,
+      z: 0,
+      toJSON: function () {
+        return this;
+      },
+    };
+  };
+
+  (SVGElement.prototype as ExtendedSVGElement).getBBox = function () {
+    return {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 20,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+      toJSON: function () {
+        return this;
+      },
+    };
+  };
+});
+
 describe("gantt", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
