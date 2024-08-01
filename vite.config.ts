bc9fb349574e9a8963234b16808eca9c1ad27dcc
Nikolaus Zolnhofer <nikolaus.zolnhofer@aisus.co.uk>
2024-08-01 13:22:17 +0100

    Update package json so now uses vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "@nikmaxott/gantt-task-react",
+      fileName: format => `@nikmaxott/gantt-task-react.${format}.js`,
+    },
+    rollupOptions: {
+      // Ensure to externalize dependencies that shouldn't be bundled
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
   },
   test: {
     globals: true,
