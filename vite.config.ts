0e81b3edc93d76f2a15719c3aa00ab8ba644113d
Nikolaus Zolnhofer <nikolaus.zolnhofer@aisus.co.uk>
2024-08-02 09:22:36 +0100

    Remove postcss plugins and add auto typing for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: "src",
+      exclude: ["src/setupTests.ts", "src/test"],
+      rollupTypes: true,
+    }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
@@ -12,7 +20,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // Ensure to externalize dependencies that shouldn't be bundled
-      external: ["react", "react/jsx-runtime"],
+      external: ["react", "react/jsx-runtime", "react-dom"],
     },
   },
   test: {
