a7fcbf04f093c1ae2a89c5d9e48e4d56bbdf6042
Nikolaus-Maxim Zolnhofer <nikolaus14@k-c13.org>
2026-08-01 13:13:40 +0100

    Add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import dts from "unplugin-dts/vite";
+import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
   plugins: [
     react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
     dts({
       include: "src",
       bundleTypes: true,
